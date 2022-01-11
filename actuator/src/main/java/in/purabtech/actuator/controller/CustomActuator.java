9e012b8fc5416a15ee03d8606f4da91f34323ce9
purab <purabdk@yahoo.co.in>
2022-01-11 21:31:49 +0530

    yml loaded...

--- actuator/src/main/java/in/purabtech/actuator/controller/CustomActuator.java
+++ actuator/src/main/java/in/purabtech/actuator/controller/CustomActuator.java
@@ -1,16 +1,11 @@
 package in.purabtech.actuator.controller;
 
-import in.purabtech.actuator.YamlPropertySourceFactory;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Endpoint(id = "customActuator")
-@ConfigurationProperties(prefix = "yaml")
-@PropertySource(value = "classpath:application.yml",factory = YamlPropertySourceFactory.class)
 public class CustomActuator {
 
     /* readoperation maps to http get
--- actuator/src/main/java/in/purabtech/actuator/controller/MyTestController.java
+++ actuator/src/main/java/in/purabtech/actuator/controller/MyTestController.java
@@ -1,10 +1,15 @@
 package in.purabtech.actuator.controller;
 
+import in.purabtech.actuator.YamlPropertySourceFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:application.yml",factory = YamlPropertySourceFactory.class)
 public class MyTestController {
 
     @Value("${server.port}")

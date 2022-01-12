75f5e94f877c1f8d4558b39c76c064038c3440a6
purab <purabdk@yahoo.co.in>
2022-01-12 15:15:58 +0530

    update code...

--- hibernate-spring/src/main/java/in/purabtech/hibernatespring/HibernateSpringApplication.java
+++ hibernate-spring/src/main/java/in/purabtech/hibernatespring/HibernateSpringApplication.java
@@ -1,13 +1,34 @@
 package in.purabtech.hibernatespring;
 
+import java.util.Optional;
+
+import in.purabtech.hibernatespring.entity.EmployeeEntity;
+import in.purabtech.hibernatespring.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class HibernateSpringApplication {
+public class HibernateSpringApplication implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    EmployeeRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(HibernateSpringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Optional<EmployeeEntity> emp = repository.findById(2L);
+
+        logger.info("Employee id 2 -> {}", emp.get());
+    }
 }
---
+++ hibernate-spring/src/main/java/in/purabtech/hibernatespring/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package in.purabtech.hibernatespring.repository;
+
+import in.purabtech.hibernatespring.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
+
+}

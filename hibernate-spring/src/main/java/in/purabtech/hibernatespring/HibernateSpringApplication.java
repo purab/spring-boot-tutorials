3ab72e8f574607c0fe6fd98b04ad914c9e76f32a
purab <purabdk@yahoo.co.in>
2022-01-12 14:33:22 +0530

    first commit

---
+++ hibernate-spring/src/main/java/in/purabtech/hibernatespring/HibernateSpringApplication.java
@@ -0,0 +1,13 @@
+package in.purabtech.hibernatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateSpringApplication.class, args);
+    }
+
+}
---
+++ hibernate-spring/src/test/java/in/purabtech/hibernatespring/HibernateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package in.purabtech.hibernatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

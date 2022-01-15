e2b8c7b9c7c2af6d31114446911176d65672bf6d
purab <purabdk@yahoo.co.in>
2022-01-15 13:17:20 +0530

    code example...

--- java-8-examples/src/MapReduceDemo.java
+++ java-8-examples/src/MapReduceDemo.java
@@ -7,6 +7,8 @@ public class MapReduceDemo {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(3,7,933,99,58);
 
+        List<String> words = Arrays.asList("purab","changu","mangu","bangu");
+
         int sum=0;
         for (int no:numbers){
             sum=sum+no;
@@ -21,5 +23,23 @@ public class MapReduceDemo {
 
         Optional<Integer> reduceSum2 = numbers.stream().reduce(Integer::sum);
         System.out.println(reduceSum2.get());
+
+        //multiplication
+        Integer reduceMulti = numbers.stream().reduce(1, (a, b) -> a * b);
+        System.out.println(reduceMulti);
+
+        //return max value
+        Integer reduceMaxVal = numbers.stream().reduce(0, (a, b) -> a > b ? a : b);
+        System.out.println(reduceMaxVal);
+
+        //return min value
+        Integer reduceMinVal = numbers.stream().reduce(Integer::min).get();
+        System.out.println(reduceMinVal);
+
+        //find longest string
+        Optional<String> longestString = words.stream().reduce((word1, word2) -> word1.length() > word2.length() ? word1 : word2);
+        System.out.println(longestString.get());
+
+
     }
 }

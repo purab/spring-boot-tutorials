4372f01c6cc35a884cf2fb0bf67ac6d9866b40fc
purab <purabdk@yahoo.co.in>
2022-01-17 17:55:41 +0530

    add mongodb example...

--- spring-mongodb/src/main/java/in/purabtech/mongodb/api/resource/BookController.java
+++ spring-mongodb/src/main/java/in/purabtech/mongodb/api/resource/BookController.java
@@ -1,14 +1,13 @@
 package in.purabtech.mongodb.api.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import in.purabtech.mongodb.api.model.Book;
 import in.purabtech.mongodb.api.repository.BookRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -23,8 +22,20 @@ public class BookController {
 		
 	}
 
+	@GetMapping("/findAllBooks")
 	public List<Book> getBooks() {
 		return bookRepository.findAll();
+	}
+
+	@GetMapping("/findAllBooks/{bookid}")
+	public Optional<Book> getBook(@PathVariable int bookid) {
+		return bookRepository.findById(bookid);
+	}
+
+	@DeleteMapping("/delete/{bookid}")
+	public String deleteBook(@PathVariable int bookid){
+		bookRepository.deleteById(bookid);
+		return "Book deleted by id:"+bookid;
 	}
 
 }

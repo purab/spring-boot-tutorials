080b76553cbe11b220f6059c0b1441c52a278b56
purab <purabdk@yahoo.co.in>
2022-01-19 21:33:07 +0530

    add role example...

---
+++ group-demo/src/main/java/in/purabtech/groupdemo/GroupDemoApplication.java
@@ -0,0 +1,13 @@
+package in.purabtech.groupdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupDemoApplication.class, args);
+    }
+
+}
---
+++ group-demo/src/main/java/in/purabtech/groupdemo/entity/User.java
@@ -0,0 +1,28 @@
+package in.purabtech.groupdemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "USER_AUTH_TBL")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String username;
+    private String password;
+    private boolean active;
+    private String roles;
+}
